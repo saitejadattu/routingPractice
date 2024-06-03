947d8b048b2589f82d14a73fbe0aef13bdd287ed
saitejadattu <nxtwave@ip-10-2-70-177.ap-south-1.compute.internal>
2024-06-03 09:37:43 +0000

    routingPractice

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/Contact" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
